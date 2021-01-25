f0ac0bfae9fafa57fc6006382c11c4f69d247b04
jetbot <jetbot@nano-4gb-jp441.localdomain>
2021-01-24 22:03:22 -0800

    first commit for completed project after test

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,6 +10,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
+    
+    this->start_node = &(this->m_Model.FindClosestNode(start_x, start_y));
+    this->end_node = &(this->m_Model.FindClosestNode(end_x, end_y));
 
 }
 
@@ -20,6 +23,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 // - Node objects have a distance method to determine the distance to another node.
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
+    return node->distance(*(this->end_node));
 
 }
 
@@ -32,6 +36,19 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
+    current_node->FindNeighbors();
+
+    for (auto neighbor : current_node->neighbors){
+
+        if(neighbor->visited){
+            continue;
+        }
+        neighbor->parent = current_node;
+        neighbor->h_value = this->CalculateHValue(neighbor);
+        neighbor->g_value = current_node->g_value + neighbor->distance(*current_node);
+        neighbor->visited = true;
+        this->open_list.push_back(neighbor);
+    }
 
 }
 
@@ -43,8 +60,15 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 // - Remove that node from the open_list.
 // - Return the pointer.
 
-RouteModel::Node *RoutePlanner::NextNode() {
+bool Compare(const RouteModel::Node* n1, const RouteModel::Node* n2) {
+    return (n1->g_value + n1->h_value) > (n2->g_value + n2->h_value);
+}
 
+RouteModel::Node *RoutePlanner::NextNode() {
+    sort(this->open_list.begin(), this->open_list.end(), Compare);
+    RouteModel::Node* pointer_to_node_lowset_f = this->open_list.back();
+    this->open_list.pop_back();
+    return pointer_to_node_lowset_f;
 }
 
 
@@ -56,12 +80,25 @@ RouteModel::Node *RoutePlanner::NextNode() {
 // - The returned vector should be in the correct order: the start node should be the first element
 //   of the vector, the end node should be the last element.
 
+
 std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
     // Create path_found vector
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
-
+    
     // TODO: Implement your solution here.
+    if(current_node != this->end_node){
+        std::cout << "ConstructFinalPath Failed, the initial node is not end node";
+    }
+    path_found.push_back(*current_node); // the current node should be the end node here
+    
+    while(current_node != this->start_node) {
+        distance += current_node->distance(*current_node->parent);
+        path_found.push_back(*current_node->parent);
+        current_node = current_node->parent;
+    }
+    
+    std::reverse(path_found.begin(), path_found.end());
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
@@ -80,5 +117,22 @@ void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
 
     // TODO: Implement your solution here.
+    current_node = this->start_node;
+    this->open_list.push_back(current_node);
+    current_node->visited = true;
+    
+    while (current_node != this->end_node){
+        
+        this->AddNeighbors(current_node);
+        current_node = this->NextNode(); 
+    }
 
+    if(current_node == this->end_node){
+        this->m_Model.path = ConstructFinalPath(current_node);
+    }
+    else{
+        std::cout << "Search Failed, unable to return the correct path";
+    }
+
+    
 }
